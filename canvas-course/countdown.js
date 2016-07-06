acec19216d6056c65034f24e2fac34103182ae17
liangfuru <328100838@qq.com>
2016-07-06 19:27:26 +0800

    del

--- canvas-course/countdown.js
+++ canvas-course/countdown.js
@@ -4,7 +4,7 @@ var RADIUS = 8;
 var MARGIN_TOP = 60;
 var MARGIN_LEFT = 30;
 
-const endTime = new Date(2016,6,9,18,47,52);
+const endTime = new Date(2016,6,10,18,47,52);
 var curShowTimeSeconds = 0
 
 var balls = [];
